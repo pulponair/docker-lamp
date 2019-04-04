5bc4c984938d826809231876224f60f4d5bbf854
Nikolas Hagelstein <nikolas.hagelstein@gmail.com>
2019-04-04 23:43:17 +0200

    [FEATURE] Refactored dlamp script added php version flag

--- bin/dlamp.sh
+++ bin/dlamp.sh
@@ -1,47 +1,43 @@
 #!/bin/bash
 usage() {
-      echo "Usage:"
-      echo "    dlamp -h                      Display this help message."
-      echo "    dlamp php <command>           Execute PHP (latest Version - 7.2)"
-      echo "    dlamp php71 <command>         Execute PHP 7.1"
-      echo "    dlamp php70 <command>         Execute PHP 7.0"
-      echo "    dlamp php56 <command>         Execute PHP 5.6"
-      exit 0
+    echo "Usage:"
+    echo "    dlamp -h                                  Display this help message."
+    echo "    dlamp php [-72|-71|-70|-56] <command>     Execute PHP"
+    exit 0
+}
+
+get_php_version() {
+    local php_version=72
+    local valid_php_versions=(72 71 70 56)
+
+    if [[ " ${valid_php_versions[*]} " == *"${1:1:2}"* ]]; then
+        php_version=${1:1:2}
+    fi
+
+    echo $php_version
 }
 
 while getopts ":h" opt; do
-  case ${opt} in
-    h )
-      usage
-      ;;
-    \? )
-      echo "Invalid Option: -$OPTARG" 1>&2
-      exit 1
-      ;;
-  esac
+    case ${opt} in
+        h )
+            usage
+            ;;
+        \? )
+            echo "Invalid Option: -$OPTARG" 1>&2
+            exit 1
+            ;;
+    esac
 done
 shift $((OPTIND -1))
 
 subcommand=$1; shift
 case "$subcommand" in
-  php )
-    echo "$subcommand $1"
-    docker run --rm -v /$(pwd):/work -w //work docker-lamp_php72:latest php $1
-    ;;
-  php71 )
-    echo "$subcommand $1"
-    docker run --rm -v /$(pwd):/work -w //work docker-lamp_php71:latest php $1
-    ;;
-  php70 )
-    echo "$subcommand $1"
-    docker run --rm -v /$(pwd):/work -w //work docker-lamp_php70:latest php $1
-    ;;
-  php56 )
-    echo "$subcommand $1"
-    docker run --rm -v /$(pwd):/work -w //work docker-lamp_php56:latest php $1
-    ;;
-  * )
-    usage
-    ;;
+    php )
+        php_version=$(get_php_version $1); shift
+        docker run --rm -v /$(pwd):/work -w //work docker-lamp_php$php_version:latest php "$*"
+        ;;
+    * )
+        usage
+        ;;
 esac
 
